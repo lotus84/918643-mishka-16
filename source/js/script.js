1d86d902b9e9b31e21e11915b0df3d0695ee69e3
Elena Mikirtumova <mikirtumova84@gmail.com>
2019-04-25 13:26:04 +0400

    Добавила декоративные элементы блоку catalog + дописала к нему скрипт

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,10 @@
 var navMain = document.querySelectorAll(".main-nav__wrap");
 var navToggle = document.querySelector(".main-nav__toggle");
-var itemBuy = document.querySelector(".product-main__checkout");
+var itemWeekBuy = document.querySelector(".product-main__checkout");
 var itemPopup = document.querySelector(".modal-item");
+var itemBuy = document.querySelectorAll(".catalog__button-buy");
+
+console.log(itemBuy);
 
 for (var i = 0; i < navMain.length; i++) {
   navMain[i].classList.remove("main-nav__wrap--nojs");
@@ -19,10 +22,13 @@ navToggle.addEventListener("click", function() {
   };
 });
 
-itemBuy.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  itemPopup.classList.add("modal-show");
-});
+for (var j = 0; j < itemBuy.length; j++) {
+  itemBuy[j].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    itemPopup.classList.add("modal-show");
+  });
+};
+
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
@@ -31,4 +37,9 @@ window.addEventListener("keydown", function (evt) {
       itemPopup.classList.remove("modal-show");
     }
   }
+});
+
+itemWeekBuy.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  itemPopup.classList.add("modal-show");
 });
