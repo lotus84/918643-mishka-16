460e716165956b19b60708f444ed6a78574bc679
Elena Mikirtumova <mikirtumova84@gmail.com>
2019-05-03 20:32:42 +0400

    Оживила слайдер блока reviews и исправила ошибку JS в консоле

--- source/js/script.js
+++ source/js/script.js
@@ -38,7 +38,36 @@ window.addEventListener("keydown", function (evt) {
   }
 });
 
-itemWeekBuy.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  itemPopup.classList.add("modal__show");
-});
+if (itemWeekBuy) {
+  itemWeekBuy.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    itemPopup.classList.add("modal__show");
+  });
+}
+
+
+// Оживление слайдера на главной странице
+
+
+var slides = document.getElementsByClassName("slider__item-wrap");
+
+if (slides) {
+  var slideIndex = 1;
+  showSlides(slideIndex);
+
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slider__item-wrap");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  }
+}
